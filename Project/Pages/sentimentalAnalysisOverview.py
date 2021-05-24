7dbcaac31097735c3fda3ae377240afcf4b88146
VaishnavMenon <47454930+VaishnavMenon@users.noreply.github.com>
2021-05-24 14:05:47 +0530

    Sentimental exception handling

--- Project/Pages/sentimentalAnalysisOverview.py
+++ Project/Pages/sentimentalAnalysisOverview.py
@@ -25,15 +25,18 @@ def startSentimentalAnalysis():
     if constants.TICKER_TO_PREDICT not in constants.TICKER_SET:
         constants.TICKER_SET.append(constants.TICKER_TO_PREDICT)
     if st.sidebar.button(label='Analyze', help='Click to start sentimental analysis, it may take some time'):
-        # st.plotly_chart(FinvizSentimentalAnalysis.finvizSentimentalAnalysis(constants.TICKER_SET))
-        finvizMeanScores = FinvizSentimentalAnalysis.finvizSentimentalAnalysis(
-            constants.TICKER_SET)
-        finviz, twitter = st.beta_columns(2)
-        with finviz:
-            st.markdown('### Finviz Scores')
-            for i in range(len(finvizMeanScores.index)):
-                st.write(
-                    f'{finvizMeanScores.index[i]} : {finvizMeanScores[finvizMeanScores.index[i]]}')
-        with twitter:
-            st.markdown('### Twitter')
-            st.write(TwitterSentimentalAnalysis.twitterSentimentalAnalysis())
+        try:
+            # st.plotly_chart(FinvizSentimentalAnalysis.finvizSentimentalAnalysis(constants.TICKER_SET))
+            finvizMeanScores = FinvizSentimentalAnalysis.finvizSentimentalAnalysis(
+                constants.TICKER_SET)
+            finviz, twitter = st.beta_columns(2)
+            with finviz:
+                st.markdown('### Finviz Scores')
+                for i in range(len(finvizMeanScores.index)):
+                    st.write(
+                        f'{finvizMeanScores.index[i]} : {finvizMeanScores[finvizMeanScores.index[i]]}')
+            #with twitter:
+                #st.markdown('### Twitter')
+                #st.write(TwitterSentimentalAnalysis.twitterSentimentalAnalysis())
+        except:
+            st.error("Please provide a valid ticker !") 
