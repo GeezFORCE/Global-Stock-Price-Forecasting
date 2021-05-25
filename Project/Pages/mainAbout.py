ea022f539a904f69ba1115096fca65192316f30a
Skifo <39179464+Skifo@users.noreply.github.com>
2021-05-25 10:09:41 +0530

    update mainAbout.py

--- Project/Pages/mainAbout.py
+++ Project/Pages/mainAbout.py
@@ -3,33 +3,14 @@
 
 # External Imports
 import streamlit as st
+import codecs
+import streamlit.components.v1 as stc
 
-def mainAbout():
-    st.markdown('## Created by ')
-    GeezFORCE, cjalvin24 = st.beta_columns(2)
-    vaishnavmenon99, skifobabu123 = st.beta_columns(2)
-    with GeezFORCE:
-        st.markdown('#### C G Giridhar')
-        st.markdown('### &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; [![Giridhar-LinkedIn](https://img.shields.io/badge/Giridhar-LinkedIn-blue?style=for-the-badge&logo=Linkedin)](https://www.linkedin.com/in/c-g-giridhar-887b00173/)')
-        st.markdown('### &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; [![Giridhar-Github](https://img.shields.io/badge/Giridhar-Github-black?style=for-the-badge&logo=Github)](https://github.com/GeezFORCE)')
-        st.markdown('### &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; [![Giridhar-Gitlab](https://img.shields.io/badge/Giridhar-Gitlab-orange?style=for-the-badge&logo=Gitlab)](https://gitlab.com/GeezFORCE)')
-        st.write('\n')
-    with cjalvin24 :
-        st.markdown('#### C J Alvin')
-        st.markdown('### [![Alvin-LinkedIn](https://img.shields.io/badge/Alvin-LinkedIn-blue?style=for-the-badge&logo=Linkedin)](https://www.linkedin.com/in/cjalvin24)')
-        st.markdown('### [![Alvin-Github](https://img.shields.io/badge/Alvin-Github-black?style=for-the-badge&logo=Github)](https://github.com/AlvinCJ)')
-        st.markdown('### [![Alvin-Gitlab](https://img.shields.io/badge/Alvin-Gitlab-orange?style=for-the-badge&logo=Gitlab)](https://gitlab.com/cjalvin24)')
-        st.write('\n')
-    with skifobabu123:
-        st.markdown('#### Skifo Babu')
-        st.markdown('### [![Skifo-LinkedIn](https://img.shields.io/badge/Skifo-LinkedIn-blue?style=for-the-badge&logo=Linkedin)](https://www.linkedin.com/in/skifo-babu-3860a0192)')
-        st.markdown('### [![Skifo-Github](https://img.shields.io/badge/Skifo-Github-black?style=for-the-badge&logo=Github)](https://github.com/Skifo)')
-        st.markdown('### [![Skifo-Gitlab](https://img.shields.io/badge/Skifo-Gitlab-orange?style=for-the-badge&logo=Gitlab)](https://gitlab.com/skifobabu123)')
-    with vaishnavmenon99:
-        st.markdown('#### Vaishnav S Menon')
-        st.markdown('### &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; [![Vaishnav-LinkedIn](https://img.shields.io/badge/Vaishnav-LinkedIn-blue?style=for-the-badge&logo=Linkedin)](https://www.linkedin.com/in/vaishnav-menon-0024a61a4/)')
-        st.markdown('### &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; [![Vaishnav-Github](https://img.shields.io/badge/Vaishnav-Github-black?style=for-the-badge&logo=Github)](https://github.com/VaishnavMenon)')
-        st.markdown('### &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; [![Vaishnav-Gitlab](https://img.shields.io/badge/Vaishnav-Gitlab-orange?style=for-the-badge&logo=Gitlab)](https://gitlab.com/vaishnavmenon99)')
+def mainAbout(about_html,width=700,height=575):
+    about_file=codecs.open(about_html,'r')
+    page=about_file.read()
+    stc.html(page,width=width,height=height,scrolling=False)
+    
 
     st.write('\n')
     st.markdown('## Gitlab : [![Gitlab](https://img.shields.io/badge/Made_With-Gitlab-orange?style=for-the-badge&logo=Gitlab)](https://gitlab.com/GeezFORCE/mainproject)')
