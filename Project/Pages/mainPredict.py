625bad1e6eb9024ddf311cd15073e7cc1e1dd3e5
C G Giridhar <girigov39@gmail.com>
2021-05-25 12:26:27 +0530

    Fix File Upload if able

--- Project/Pages/mainPredict.py
+++ Project/Pages/mainPredict.py
@@ -45,3 +45,45 @@ def mainPredict():
         with st.spinner("Forecasting In Progress"):
             getForecast(constants.PERIOD_TO_FORECAST,
                     selectedModelName, selectedModelWeights)
+
+
+    # Code for Custom model uploading encounter UTF-8 Error, fix if can
+
+    # st.caption("Number of tickers must be same as that of the model")
+    # selectedOption = st.radio(label='Model Selector', 
+    #                           options=['Choose From List', 'Upload Custom'], 
+    #                           index=1, 
+    #                           help='Select default to choose from pretrained models and Custom to upload locally trained model')
+    # if selectedOption == 'Choose From List':
+    #     selectedModelName = st.selectbox(label='Select Model', options=glob.glob(
+    #         'savedModelInformation/modelInformation/*.json', recursive=True), index=0, help='Selecting Model used for Forecast')
+    #     selectedModelWeights = st.selectbox(label='Select Model Weights', options=glob.glob(
+    #         'savedModelInformation/modelWeights/*.h5', recursive=True), index=0, help='Selecting Weights of the model used for Forecast')
+    #     if st.sidebar.button(label='Forecast', help='Click to start Forecasting, it may take some time'):
+    #         with st.spinner("Forecasting In Progress"):
+    #             getForecast(constants.PERIOD_TO_FORECAST,
+    #                     selectedModelName, selectedModelWeights)
+    # else:
+    #     fileList = st.file_uploader(label='Upload Model Information', type=['json', 'h5'], accept_multiple_files=True, help='Upload Saved JSON and h5 files of the trained model')
+    #     if fileList != 'None' and len(fileList) == 2:
+    #         fileNames = [i.name for i in fileList]
+    #         newFileList = []
+    #         for file in fileList:
+    #             convertedFile = file.getvalue()
+    #             convertedFile = convertedFile.decode('utf8')
+    #             newFileList.append(convertedFile)
+    #         if ((fileNames[0].split('-')[-1]).split('.')[0] == (fileNames[1].split('-')[-1]).split('.')[0]) and ((fileNames[0].split('-')[-1]).split('.')[1] != (fileNames[1].split('-')[-1]).split('.')[1]):
+    #             if (fileNames[0].split('-')[-1]).split('.')[1] == "json":
+    #                 selectedModelName = newFileList[0]
+    #                 selectedModelWeights = newFileList[1]
+    #             else:
+    #                 selectedModelName = newFileList[1]
+    #                 selectedModelWeights = newFileList[0]
+    #             # st.write(selectedModelName, selectedModelWeights)
+    #             if st.sidebar.button(label='Forecast', help='Click to start Forecasting, it may take some time'):
+    #                 with st.spinner("Forecasting In Progress"):
+    #                     getForecast(constants.PERIOD_TO_FORECAST,
+    #                             selectedModelName, selectedModelWeights)
+    #         else:
+    #             st.warning('Files uploaded are not compatible! Upload Again')
+
