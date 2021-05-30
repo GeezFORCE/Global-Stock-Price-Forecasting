1b3ebbfb4d33ab7ba25e865a5568e4ae44a23875
VaishnavMenon <47454930+VaishnavMenon@users.noreply.github.com>
2021-05-30 22:17:41 +0530

    Add custom model upload

--- Project/Pages/mainPredict.py
+++ Project/Pages/mainPredict.py
@@ -4,10 +4,13 @@
 # External Imports
 import streamlit as st
 import glob
+import json
 
 # Internal Imports
 from Project import constants
 from Project.Forecast import getForecast
+from Project.Forecast import getuploadedForecast
+from Project.Forecast import getuploadedForecast2
 
 
 # Function to take inputs and call forecasting method
@@ -36,54 +39,38 @@ def mainPredict():
     '''
 
     st.caption("Number of tickers must be same as that of the model")
-    selectedModelName = st.selectbox(label='Select Model', options=glob.glob(
-        'savedModelInformation/modelInformation/*.json', recursive=True), index=0, help='Selecting Model used for Forecast')
-    selectedModelWeights = st.selectbox(label='Select Model Weights', options=glob.glob(
-        'savedModelInformation/modelWeights/*.h5', recursive=True), index=0, help='Selecting Weights of the model used for Forecast')
-    if st.sidebar.button(label='Forecast', help='Click to start Forecasting, it may take some time'):
-        st.sidebar.write('Forecast Started')
-        with st.spinner("Forecasting In Progress"):
-            getForecast(constants.PERIOD_TO_FORECAST,
-                    selectedModelName, selectedModelWeights)
+    selectedOption = st.radio(label='Model Selector', 
+                              options=['Choose From List','Upload Custom Model: Model and weights separately' ,'Upload Custom Model: Model and weights together'], 
+                              index=0, 
+                              help='Select default to choose from pretrained models and Custom to upload locally trained model')
+    if selectedOption == 'Choose From List':
+        selectedModelName = st.selectbox(label='Select Model', options=glob.glob(
+            'savedModelInformation/modelInformation/*.json', recursive=True), index=0, help='Selecting Model used for Forecast')
+        selectedModelWeights = st.selectbox(label='Select Model Weights', options=glob.glob(
+            'savedModelInformation/modelWeights/*.h5', recursive=True), index=0, help='Selecting Weights of the model used for Forecast')
+        if st.sidebar.button(label='Forecast', help='Click to start Forecasting, it may take some time'):
+            with st.spinner("Forecasting In Progress"):
+                getForecast(constants.PERIOD_TO_FORECAST,
+                        selectedModelName, selectedModelWeights)
 
+    elif selectedOption == 'Upload Custom Model: Model and weights separately':
+        uploadedmodel = st.file_uploader(label='Upload Model', type=['json'], accept_multiple_files=False, help='Upload Saved JSON files of the trained model')
+        uploadedweight = st.file_uploader(label='Upload Weight (.h5py.zip)', type='zip', accept_multiple_files=False, help='Upload Saved h5 files of the trained model as zipped file')
+        if(uploadedmodel!=None and uploadedweight!=None):
+            if st.sidebar.button(label='Forecast', help='Click to start Forecasting, it may take some time'):
+                with st.spinner("Forecasting In Progress"):
+                    getuploadedForecast(constants.PERIOD_TO_FORECAST,uploadedmodel,uploadedweight)
+        else:
+            st.error("Waiting for files to be uploaded.")
 
-    # Code for Custom model uploading encounter UTF-8 Error, fix if can
-
-    # st.caption("Number of tickers must be same as that of the model")
-    # selectedOption = st.radio(label='Model Selector', 
-    #                           options=['Choose From List', 'Upload Custom'], 
-    #                           index=1, 
-    #                           help='Select default to choose from pretrained models and Custom to upload locally trained model')
-    # if selectedOption == 'Choose From List':
-    #     selectedModelName = st.selectbox(label='Select Model', options=glob.glob(
-    #         'savedModelInformation/modelInformation/*.json', recursive=True), index=0, help='Selecting Model used for Forecast')
-    #     selectedModelWeights = st.selectbox(label='Select Model Weights', options=glob.glob(
-    #         'savedModelInformation/modelWeights/*.h5', recursive=True), index=0, help='Selecting Weights of the model used for Forecast')
-    #     if st.sidebar.button(label='Forecast', help='Click to start Forecasting, it may take some time'):
-    #         with st.spinner("Forecasting In Progress"):
-    #             getForecast(constants.PERIOD_TO_FORECAST,
-    #                     selectedModelName, selectedModelWeights)
-    # else:
-    #     fileList = st.file_uploader(label='Upload Model Information', type=['json', 'h5'], accept_multiple_files=True, help='Upload Saved JSON and h5 files of the trained model')
-    #     if fileList != 'None' and len(fileList) == 2:
-    #         fileNames = [i.name for i in fileList]
-    #         newFileList = []
-    #         for file in fileList:
-    #             convertedFile = file.getvalue()
-    #             convertedFile = convertedFile.decode('utf8')
-    #             newFileList.append(convertedFile)
-    #         if ((fileNames[0].split('-')[-1]).split('.')[0] == (fileNames[1].split('-')[-1]).split('.')[0]) and ((fileNames[0].split('-')[-1]).split('.')[1] != (fileNames[1].split('-')[-1]).split('.')[1]):
-    #             if (fileNames[0].split('-')[-1]).split('.')[1] == "json":
-    #                 selectedModelName = newFileList[0]
-    #                 selectedModelWeights = newFileList[1]
-    #             else:
-    #                 selectedModelName = newFileList[1]
-    #                 selectedModelWeights = newFileList[0]
-    #             # st.write(selectedModelName, selectedModelWeights)
-    #             if st.sidebar.button(label='Forecast', help='Click to start Forecasting, it may take some time'):
-    #                 with st.spinner("Forecasting In Progress"):
-    #                     getForecast(constants.PERIOD_TO_FORECAST,
-    #                             selectedModelName, selectedModelWeights)
-    #         else:
-    #             st.warning('Files uploaded are not compatible! Upload Again')
-
+    else:
+        
+        stream = st.file_uploader('TF.Keras model file (.h5py.zip)', type='zip')
+        if(stream!=None):
+        
+            if st.sidebar.button(label='Forecast', help='Click to start Forecasting, it may take some time'):
+                with st.spinner("Forecasting In Progress"):
+                    getuploadedForecast2(constants.PERIOD_TO_FORECAST,stream)
+        else:
+             st.error("Waiting for file to be uploaded.")
+       
